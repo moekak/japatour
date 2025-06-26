1ae09ece10a5d0a3b63ac256e115fdae46d0c3cc
moekak <moeka.k0729@gmail.com>
2025-06-26 20:08:25 +0900

    修正

--- resources/js/components/elementOperation2/itinerary/DupeItinerary.js
+++ resources/js/components/elementOperation2/itinerary/DupeItinerary.js
@@ -1,41 +1,41 @@
-import ItineraryInterface from "./ItineraryInterface.js"
+import ItineraryInterface from "./ItineraryInterface.js";
 
 export default class DupeItinerary extends ItineraryInterface {
-      constructor(){
-            super(); 
-            this.template = document.getElementById("itinerary-template").innerHTML
-            this.wrapper = document.getElementById("itinerary-wrapper")
-      }
+    constructor() {
+        super();
+        this.template = document.getElementById("itinerary-template").innerHTML;
+        this.wrapper = document.getElementById("itinerary-wrapper");
+    }
 
+    /**
+     * @override
+     */
+    duplicateElement() {
+        this.activityIndex++;
+        this.itineraryIndex++;
+        this.createDOM();
+    }
 
-      /**
-       * @override
-       */
-      duplicateElement(){
-            this.activityIndex ++
-            this.itineraryIndex ++
-            this.createDOM()
-            
-      }
+    /**
+     * @override
+     */
+    createDOM() {
+        let rawHTML = this.template
+            .replace(/{itinerary_index}/g, this.itineraryIndex)
+            .replace(/{activity_index}/g, this.activityIndex)
+            .replace(/{itinerary_count}/g, this.itineraryIndex + 1);
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("itinerary-item");
+        newDiv.datasetId = this.itineraryIndex;
+        newDiv.innerHTML = rawHTML;
+        this.wrapper.appendChild(newDiv);
+    }
 
-      /**
-       * @override
-       */
-      createDOM(){
-            let rawHTML = this.template.replace(/{itinerary_index}/g, this.itineraryIndex).replace(/{activity_index}/g, this.activityIndex).replace(/{itinerary_count}/g, this.itineraryIndex +1);
-            const newDiv = document.createElement("div")
-            newDiv.classList.add("itinerary-item")
-            newDiv.datasetId = this.itineraryIndex
-            newDiv.innerHTML = rawHTML
-            this.wrapper.appendChild(newDiv)
-      }
-
-      
-      /**
-       * @override
-       */
-      deleteElement(button){
-            const targetElement = button.closest(".itinerary-item")
-            this.wrapper.removeChild(targetElement)
-      }
+    /**
+     * @override
+     */
+    deleteElement(button) {
+        const targetElement = button.closest(".itinerary-item");
+        this.wrapper.removeChild(targetElement);
+    }
 }
--- resources/js/components/elementOperation2/itinerary/data/IndexManager.js
+++ resources/js/components/elementOperation2/itinerary/data/IndexManager.js
@@ -1,20 +1,20 @@
 // シングルトンパターン
-class IndexManager{
-      constructor(){
-            if (!IndexManager.instance) {
-                  this.list = {}; // 初期状態
-                  IndexManager.instance = this; // インスタンスを保存
-            }
-            return IndexManager.instance; // 常に同じインスタンスを返す
-      }
+class IndexManager {
+    constructor() {
+        if (!IndexManager.instance) {
+            this.list = []; // 初期状態
+            IndexManager.instance = this; // インスタンスを保存
+        }
+        return IndexManager.instance; // 常に同じインスタンスを返す
+    }
 
-      getState(){
-            return this.list
-      }
+    getItineraryCount(itineraryCount) {
+        return this.list[itineraryCount];
+    }
 
-      setState(value){
-            this.list.push(value)
-      }
+    setItinerayCount(itineraryCount) {
+        this.list.push(itineraryCount);
+    }
 }
 
 const indexManager = new IndexManager();
