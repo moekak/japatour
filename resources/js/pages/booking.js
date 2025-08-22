2222bebf220069e46295dfcb5ca25d6b0731cdc4
moekak <moeka.k0729@gmail.com>
2025-08-22 15:19:36 +0900

    ツアー予約

--- resources/js/pages/booking.js
+++ resources/js/pages/booking.js
@@ -1,17 +1,3 @@
 import BookingCalculator from "../components/tourBook/BookingCalculator.js";
 
 const bookingInstance = new BookingCalculator()
-
-const incrementAdultBtn = document.getElementById("js_increment-adult")
-const decrementAdultBtn = document.getElementById("js_decrement-adult")
-const adultCount = document.getElementById("adults-count") 
-
-incrementAdultBtn.addEventListener("click", ()=>{
-      adultCount.innerHTML = parseInt(adultCount.innerHTML) + 1
-})
-
-decrementAdultBtn.addEventListener("click", ()=>{
-      adultCount.innerHTML = parseInt(adultCount.innerHTML) - 1
-})
-
-
