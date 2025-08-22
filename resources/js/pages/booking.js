383b34b68703a599b56369bbadb18ee230c30b41
moekak <moeka.k0729@gmail.com>
2025-08-22 16:34:13 +0900

    ツアー選択機能

--- resources/js/pages/booking.js
+++ resources/js/pages/booking.js
@@ -1,3 +1,9 @@
 import BookingCalculator from "../components/tourBook/BookingCalculator.js";
 
 const bookingInstance = new BookingCalculator()
+
+
+document.getElementById('continue-to-step-2').addEventListener("click", ()=>{
+      document.getElementById("step-2-content").classList.remove("hidden")
+      document.getElementById("step-1-content").classList.remove("add")
+})
