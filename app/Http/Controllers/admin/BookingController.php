05278cba6e4a2ac58a2769efcd0bef61a818cb36
moekak <moeka.k0729@gmail.com>
2025-08-25 11:53:08 +0900

    ツアー保存

--- app/Http/Controllers/admin/BookingController.php
+++ app/Http/Controllers/admin/BookingController.php
@@ -23,7 +23,7 @@ class BookingController extends Controller
     }
 
     public function store(Request $request){
-        print_r($request);
+        print_r($request->all());
     }
 
     // public function createPaymentIntent(Request $request){
