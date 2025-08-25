aa0c9d14c6ba27e03ece9802beae96cc5e9a5505
moekak <moeka.k0729@gmail.com>
2025-08-25 11:51:57 +0900

    ツアー保存

--- resources/views/book/booking.blade.php
+++ resources/views/book/booking.blade.php
@@ -419,7 +419,7 @@
                                         <button type="button" id="back-to-step-2" class="flex-1 border-2 border-[#e7d0d0] text-[#994d4d] py-3 rounded-lg font-medium hover:border-[#994d4d] transition-colors duration-300">
                                             Back
                                         </button>
-                                        <form action="{{route("book.store")}}" method="POST" class="flex-1 bg-[#e92929] text-white py-3 rounded-lg font-medium hover:bg-[#d61f1f] transition-all duration-300">
+                                        <form action="{{route("book.store")}}" method="POST" class="flex-1 flex align-center justify-center bg-[#e92929] text-white py-3 rounded-lg font-medium hover:bg-[#d61f1f] transition-all duration-300">
                                             @csrf
                                             {{-- ツアー情報 --}}
                                             <input type="hidden" value="" id="js_itinerary_id" name="itinerary_id">
@@ -436,7 +436,7 @@
                                             <input type="hidden" value="" id="js_request" name="request">
                                             <input type="hidden" value={{$tour->title}} id="js_tour_name">
                                             <input type="hidden" value={{$tour->title}} id="js_itinerary_name">
-                                            <button type="submit" id="confirm-booking">
+                                            <button type="submit" id="confirm-booking" cla>
                                                 Confirm & Book Now
                                             </button>
                                         </form>
