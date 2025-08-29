98afb229bfee8a7def803632fe1e2612adf8ba40
moekak <moeka.k0729@gmail.com>
2025-08-29 16:27:52 +0900

    ツアー一覧

--- app/Services/Util/FormatService.php
+++ app/Services/Util/FormatService.php
@@ -14,9 +14,18 @@ class FormatService
       public static function generateStar($average){
             $star = "";
             $int = intval($average);
+            $fractional_part = fmod($average, 1);
+            $empty = 5 - $int;
             for($i = 0; $i < $int; $i ++){
                   $star .= '<i class="fas fa-star text-yellow-400"></i>';
             }
+            if($fractional_part > 0){
+                  $star .= '<i class="fas fa-star-half-alt text-yellow-400"></i>';
+            }
+            if($empty >= 1){
+                  $star .= '<i class="far fa-star text-gray-300"></i>';
+            }
+
             return $star;
       }
 
